8add9e75b81332198377a064520680610750589b
Jennifer Tchai <jentchai@gmail.com>
2021-12-18 18:53:19 -0500

    commit tree b419d188a0277e7edc86978576fae9a0344389fb

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+ changeParagraph()
+});
+
+function changeParagraph () {
+    document.querySelector("#text");
+    text.textContent = "This is really cool!";
+}
